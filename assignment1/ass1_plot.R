ae7fd49704cc73a469615f8f3ef330a8485fef5e
lucaschilleron <100561817@alumnos.uc3m.es>
2026-02-05 13:01:07 +0100

    Create ass1_plot.R

---
+++ assignment1/ass1_plot.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+gapminder <- read.csv("data/gapminder.csv")
+head(gapminder)
+str(gapminder)
+countries <- c("Spain", "France", "Germany", "United Kingdom")
+df <- gapminder[gapminder$country %in% countries, ]
+ggplot(df, aes(x = year, y = lifeExp, color = country)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Year", y = "Life expectancy",
+       title = "Life expectancy over time") +
+  theme_minimal()
+ggsave("assignment1/ass1_plot.png", width = 7, height = 5)
